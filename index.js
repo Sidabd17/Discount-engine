cc53e395c238aa8808212a24839e75d9b53f9f45
Sidabd17 <mdsajid44149@gmail.com>
2025-08-04 21:18:49 +0530

    Raw logic implemented to calculate scores and discount

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("input.json" , "utf-8"));
-console.log(data);
+// console.log(data);
 
-const maxSeniorityMonths = 0;
-const maxActiveClients = 0;
+let maxSeniorityMonths = 1;
+let maxActiveClients = 1;
 
 const salesAgents = data.salesAgents;
 for(const agent of salesAgents){
@@ -12,3 +12,44 @@ for(const agent of salesAgents){
     maxActiveClients = Math.max(maxActiveClients, agent.activeClients);
 }
 
+console.log("Max Seniority Months:", maxSeniorityMonths);
+console.log("Max Active Clients:", maxActiveClients);
+
+const normaliseSalesAgentsData = (agent) =>{
+    return {
+        id: agent.id,
+        performanceScore: agent.performanceScore / 100,
+        seniorityMonths: Math.round(agent.seniorityMonths / maxSeniorityMonths * 100) / 100,
+        targetAchievedPercent: agent.targetAchievedPercent / 100,
+        activeClients: Math.round(agent.activeClients / maxActiveClients * 100) / 100
+    };
+}
+
+const normalisedSalesAgentsDataList = salesAgents.map(agent=> normaliseSalesAgentsData(agent));
+console.log(normalisedSalesAgentsDataList);
+
+const individualScores = (agent) => {
+    return {
+        id : agent.id,
+        score : Math.round((0.4 * agent.performanceScore + 0.3 * agent.seniorityMonths + 0.2 * agent.targetAchievedPercent + 0.1 * agent.activeClients) * 100) / 100
+    }
+}
+
+const individualScoresList = normalisedSalesAgentsDataList.map(agent => individualScores(agent));
+console.log(individualScoresList);
+
+let totalScore = 0;
+for(const agent of individualScoresList){
+    totalScore += agent.score;
+}
+
+const totalDiscount = data.siteKitty ;
+
+const individualDiscounts = individualScoresList.map(agent => {
+    return{
+        id: agent.id,
+        discount: Math.round((agent.score / totalScore) * totalDiscount * 100) / 100
+    }
+})
+
+console.log(individualDiscounts);
